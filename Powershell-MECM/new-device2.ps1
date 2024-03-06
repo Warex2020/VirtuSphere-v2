18ede15887e32322728f22be528f9061f8d5794c
Warex <warex@junix.de>
2024-03-06 17:44:47 +0100

    check update

--- Powershell-MECM/new-device2.ps1
+++ Powershell-MECM/new-device2.ps1
@@ -1,100 +1,78 @@
-# Lade Variablen aus Registry
-$MECM_ProviderMachineName = (Get-ItemProperty -Path "HKLM:\SOFTWARE\VirtuSphere\MECM").MECM_ProviderMachineName
-$MECM_SiteCode = (Get-ItemProperty -Path "HKLM:\SOFTWARE\VirtuSphere\MECM").MECM_SiteCode
-$VirtuSphere_WebAPI = (Get-ItemProperty -Path "HKLM:\SOFTWARE\VirtuSphere\MECM").VirtuSphere_WebAPI
-$PowershellLogPath = (Get-ItemProperty -Path "HKLM:\SOFTWARE\VirtuSphere\MECM").PowershellLogPath
-    
-    
-    # Import    MECM Module
-    Import-Module ($env:SMS_ADMIN_UI_PATH.Substring(0,$env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1')
-    Set-Location "$($MECM_SiteCode):\"
-    
-    # JSON-Inhalt von der URL abrufen
-    try {
-        $jsonUrl = "http://"+$VirtuSphere_WebAPI + "/mecm-api.php?action=getDeviceList"
-        $MySQLDeviceList = Invoke-RestMethod -Uri $jsonUrl
-    } catch {
-        Write-Host "Failed to connect to $jsonUrl. Please check your connection and try again." -ForegroundColor Red
-        exit
+# Verbesserte Variablen-Ladung aus der Registry mit einem HashTable zur Vereinfachung
+$paths = 'HKLM:\SOFTWARE\VirtuSphere\MECM'
+$registryProps = @( 'MECM_ProviderMachineName', 'MECM_SiteCode', 'VirtuSphere_WebAPI', 'PowershellLogPath' ) | ForEach-Object {
+    @{ $_ = (Get-ItemProperty -Path $paths).$_ }
+}
+$Config = @{}
+$registryProps.ForEach({ $Config += $_ })
+
+# Import MECM Module
+$adminUIPath = $env:SMS_ADMIN_UI_PATH.Substring(0, $env:SMS_ADMIN_UI_PATH.Length-5)
+Import-Module "$adminUIPath\ConfigurationManager.psd1"
+Set-Location "$($Config.MECM_SiteCode):\"
+
+# JSON-Inhalt von der URL abrufen
+$jsonUrl = "http://${Config.VirtuSphere_WebAPI}/mecm-api.php?action=getDeviceList"
+try {
+    $MySQLDeviceList = Invoke-RestMethod -Uri $jsonUrl
+} catch {
+    Write-Host "Failed to connect to $jsonUrl. Please check your connection and try again." -ForegroundColor Red
+    exit
+}
+
+# Lade DeviceList vom MECM
+$MECMDeviceList = Get-CMDevice | Select-Object Name, MACAddress, SMSID
+
+foreach ($device in $MySQLDeviceList) {
+    $deviceName = $device.vm_name
+    $deviceMAC = ($device.interfaces | Where-Object { $_.mode -eq 'DHCP' }).mac
+    $deviceSMSID = $device.SMSID
+    $deviceOS = $device.vm_os
+    $devicePackages = $device.packages
+
+    if ($MECMDeviceList.Name -contains $deviceName) {
+        Write-Host "Skip $deviceName, bereits in MECM DB vorhanden." -ForegroundColor Cyan
+        continue
     }
-    
-    # Lade DeviceList vom MECM
-    $MECMDeviceList = Get-CMDevice | Select-Object -Property Name, MACAddress, SMSID
-    
-    ## interfaces    : {@{id=101; vm_id=58; ip=; subnet=; gateway=; dns1=; dns2=; vlan=SIDS_SRV_3_Data; mac=00:50:56:9d:0a:fe; mode=DHCP; type=}}
-    # Vergleiche die beiden Listen und zeige die fehlenden Geräte in MECMDeviceList an
-    #$MissingDevices = Compare-Object -ReferenceObject $MySQLDeviceList -DifferenceObject $MECMDeviceList -Property Name, MACAddress, SMSID -PassThru | Where-Object { $_.SideIndicator -eq "<=" }
-    
-    # Füge fehlende Geräte hinzu
-    foreach ($device in $MySQLDeviceList) {
 
-        $deviceName = $device.vm_name
-        $deviceOS = $device.vm_os
-        $deviceMAC = $null
-        $devicePackages = $device.packages
+    if (-not $deviceMAC) {
+        Write-Host "Device $deviceName has no MAC-Address. Skipping device" -ForegroundColor Yellow
+        continue
+    }
 
-            if($MECMDeviceList.Name.Contains($deviceName)){
-            write-host "skip $deviceName, weil in MECM DB drin... "
-            #continue
-            }
+    Write-Host "Adding device $deviceName with MAC $deviceMAC and SMSID $deviceSMSID to MECM" -ForegroundColor Green
 
-
-        #$deviceInterface = {@{id=101; vm_id=58; ip=; subnet=; gateway=; dns1=; dns2=; vlan=SIDS_SRV_3_Data; mac=00:50:56:9d:0a:fe; mode=DHCP; type=}}
-        $deviceInterface = $device.interfaces
-        if($deviceInterface.Count -gt 1){
-            foreach ($interface in $deviceInterface) {
-                if($interface.mode = "DHCP"){
-                    $deviceMAC = $interface.mac
-                }
-            }
-        }else{
-            $deviceMAC = $deviceInterface[0].mac}
-
-        if($null -eq $deviceMAC -or "" -eq $deviceMAC){
-            Write-Host "Device $deviceName has no MAC-Address. Skipping device" -ForegroundColor Red
-            #continue
-        }else{
-
-            $deviceSMSID = $device.SMSID
-            Write-Host "Adding device $deviceName with MAC $deviceMAC and SMSID $deviceSMSID to MECM" -ForegroundColor Green
-            #$newDevice = New-CMDevice -Name $deviceName -MacAddress $deviceMAC -SMSID $deviceSMSID
-            #Import-CMComputerInformation -ComputerName "$($hostname)" -MacAddress $($computer.macAddress) -CollectionName "$($computer.deployment)"
-            
-            if(get-CMDevice -Name $deviceName){
-                write-host "$deviceName ist bereits in der MECM Datenbank!"
-               # continue
-            }
-
-            try{
-                Import-CMComputerInformation -ComputerName $deviceName -MacAddress $deviceMAC -CollectionName "All Systems"
-            
-            }catch{
-                write-host "$deviceName konnte nicht hinzugef�gt werden. Versuch es gleich nochmal!" -ForegroundColor Red
-            }
-
-             # Betriebssystem
-            $deviceCollection = Get-CMDeviceCollection -Name $deviceOS
-            if($null -eq $deviceCollection){
-                Write-Host "Collection $deviceOS does not exist. Skipping device" -ForegroundColor Red
-                continue
-            }else{
-                try{
-                    Add-CMDeviceCollectionDirectMembershipRule -CollectionName $deviceOS -ResourceId $deviceCollection.CollectionID
-                    Invoke-CMCollectionUpdate -Name $deviceOS
-                }catch{ write-host -ForegroundColor red "$deviceOS Fehler!" }
-            }
-
-            #Packages
-            foreach($Package in $devicePackages){
-                write-host "F�ge $($Package.package_name) zu $deviceName "
-                try{
-                add-CMDeviceCollectionDirectMembershipRule -CollectionName "$($Package.package_name)" -ResourceName $deviceName
-                }catch{ write-host -ForegroundColor red "$($Package.package_name) Fehler!" }
-                Invoke-CMCollectionUpdate -Name $deviceOS
-            }
-
-            
+    # Prüfe, ob das Gerät bereits existiert, bevor es hinzugefügt wird
+    if (!(Get-CMDevice -Name $deviceName)) {
+        try {
+            Import-CMComputerInformation -ComputerName $deviceName -MacAddress $deviceMAC -CollectionName "All Systems"
+        } catch {
+            Write-Host "$deviceName konnte nicht hinzugefügt werden. Versuche es später noch einmal!" -ForegroundColor Red
+            continue
         }
-
     }
-    
+
+    # Betriebssystem-Collection
+    $deviceCollection = Get-CMDeviceCollection -Name $deviceOS
+    if ($null -eq $deviceCollection) {
+        Write-Host "Collection $deviceOS does not exist. Skipping device" -ForegroundColor Magenta
+    } else {
+        try {
+            Add-CMDeviceCollectionDirectMembershipRule -CollectionName $deviceOS -ResourceId ($deviceCollection.CollectionID)
+            Invoke-CMCollectionUpdate -Name $deviceOS
+        } catch {
+            Write-Host "$deviceOS Fehler beim Hinzufügen des Geräts zur Collection!" -ForegroundColor Red
+        }
+    }
+
+    # Pakete
+    foreach ($Package in $devicePackages) {
+        Write-Host "Adding $($Package.package_name) to $deviceName" -ForegroundColor Blue
+        try {
+            Add-CMDeviceCollectionDirectMembershipRule -CollectionName "$($Package.package_name)" -ResourceName $deviceName
+            Invoke-CMCollectionUpdate -Name $deviceOS
+        } catch {
+            Write-Host "Fehler beim Hinzufügen von $($Package.package_name) zu $deviceName" -ForegroundColor Red
+        }
+    }
+}
