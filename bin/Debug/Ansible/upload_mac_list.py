09e4a62e108a5c1a46e6c84c53a4e3047c7ed2d3
Warex <warex@junix.de>
2024-03-18 15:49:20 +0100

    upload

--- bin/Debug/Ansible/upload_mac_list.py
+++ bin/Debug/Ansible/upload_mac_list.py
@@ -1,5 +1,24 @@
-import requests
 import json
+import csv
+
+input_file_path = '/tmp/vm_infos.json'
+output_file_path = '/tmp/vm_network_details.csv'
+# URL der WebAPI
+api_url = 'http://{{apiUrl}}/db_importMAC.php?action=updateInterface'
+
+
+with open(input_file_path, 'r') as json_file:
+    vm_infos = json.load(json_file)
+
+with open(output_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['VM Name', 'Interface', 'MAC Address'])
+
+    for vm_info in vm_infos:
+        if 'instance' in vm_info and 'guest' in vm_info['instance'] and 'net' in vm_info['instance']['guest']:
+            for net_info in vm_info['instance']['guest']['net']:
+                csv_writer.writerow([vm_info['item']['vm_name'], net_info.get('network'), net_info.get('macAddress')])
+
 
 def read_file_and_send_data(file_path, api_url):
     # Datei lesen
@@ -18,10 +37,7 @@ def read_file_and_send_data(file_path, api_url):
     print(f"Status Code: {response.status_code}")
     print(f"Response Body: {response.text}")
 
-# Pfad zur Datei
-file_path = '/tmp/vm_mac_list.csv'
-# URL der WebAPI
-api_url = 'http://{{apiUrl}}/db_importMAC.php?action=updateInterface'
+
 
 # Funktion ausführen
-read_file_and_send_data(file_path, api_url)
+read_file_and_send_data(output_file_path, api_url)
